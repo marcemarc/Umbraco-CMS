d701639da98e67a71ba3ad4c15ffba8253cd803e
Marc Goodson <marc@snowmoon.co.uk>
2016-06-13 19:11:54 +0100

    Gist of the 301 redirects dashboard, in all the wrong places = needs paging, but more importantly search by redirect url (that will need adding to the service)

---
+++ src/Umbraco.Web.UI/App_Plugins/RedirectUrlDashboard/redirecturlsearch.controller.js
@@ -0,0 +1,64 @@
+﻿angular.module("umbraco").controller("Umbraco.RedirectUrlSearch", function ($scope, $http, angularHelper, notificationsService, entityResource) {
+
+    //...todo
+    //paging
+    //search by url or url part
+    //search by domain
+    //display domain in dashboard results?
+
+    $scope.isSearch = false;
+    $scope.hasResults = false;
+
+
+    $scope.search = function () {
+
+        //do we want to search by url and by domain...
+
+        var searchTerm = $scope.searchTerm;
+
+        $http.get("backoffice/api/RedirectUrlManagement/SearchRedirectUrls/?searchTerm=" + searchTerm).then(function (response) {
+            var matchingItems = response.data;   
+            $scope.isSearch = true;
+            $scope.StatusMessage = matchingItems.StatusMessage;
+            $scope.hasResults = matchingItems.HasSearchResults;
+            $scope.redirectUrls = matchingItems.SearchResults;
+            angular.forEach($scope.redirectUrls, function (item) {     
+                $http.get("backoffice/api/RedirectUrlManagement/GetPublishedUrl/?id=" + item.ContentId).then(function (response) {
+                    item.ContentUrl = response.data;
+                });               
+            });
+        });
+    };
+
+    $scope.load = function () {   
+       // $scope.searchTerm = "";
+        $scope.search();
+    };
+
+    $scope.removeRedirect = function (redirectToDelete) {
+        $http.post("backoffice/api/RedirectUrlManagement/DeleteRedirectUrl/" + redirectToDelete.Id).then(function (response) {
+            if (response.status == 200) {
+                notificationsService.success('Redirect Url Removed!', 'Redirect Url ' + redirectToDelete.Url + ' has been deleted');
+                // now remove from table client sides
+                var index = -1;
+                var urlArr = eval($scope.redirectUrls);
+                for (var i = 0; i < urlArr.length; i++) {
+                    if (urlArr[i].Id === redirectToDelete.Id) {
+                        index = i;
+                        break;
+                    }
+                }
+                if (index === -1) {
+                    notificationsService.warning('Redirect Url Removal Error!', 'Redirect Url ' + redirectToDelete.Url + ' may have already been removed');
+                }
+                $scope.redirectUrls.splice(index, 1);
+            }
+            else {
+                notificationsService.warning('Redirect Url Error!','Redirect Url ' + redirectToDelete.Url + ' was not deleted');
+            }   
+        }); 
+    };
+    $scope.load();
+});
+
+
---
+++ src/Umbraco.Web/Redirects/RedirectUrlSearchResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Models;
+
+namespace Umbraco.Web.Redirects
+{
+    public class RedirectUrlSearchResult
+    {
+        public string StatusMessage { get; set; }
+        public IEnumerable<IRedirectUrl> SearchResults { get; set; }
+        public bool HasSearchResults { get; set; }
+        public bool HasExactMatch { get; set; }
+        public long TotalCount { get; set; }
+        public int PageCount { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
